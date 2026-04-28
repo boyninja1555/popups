67b932ca5f070d19d3d29d147f0bd3a56afa9049
boyninja1555 <flappyfloorg@gmail.com>
2026-04-27 23:54:41 -0500

    Am I stoopid?

--- popups.js
+++ popups.js
@@ -1,17 +1,15 @@
-(()=>{
-	function newPopup(url, tagline, imageUrl) {
-		const popup = document.createElement("a")
-		popup.href = url
-		popup.target = "_blank"
+function newPopup(url, tagline, imageUrl) {
+	const popup = document.createElement("a")
+	popup.href = url
+	popup.target = "_blank"
 
-		const imageEl = document.createElement("img")
-		imageEl.src = imageUrl
-		popup.appendChild(imageEl)
+	const imageEl = document.createElement("img")
+	imageEl.src = imageUrl
+	popup.appendChild(imageEl)
 
-		const taglineEl = document.createElement("span")
-		taglineEl.textContent = tagline
-		popup.appendChild(taglineEl)
+	const taglineEl = document.createElement("span")
+	taglineEl.textContent = tagline
+	popup.appendChild(taglineEl)
 
-		document.body.appendChild(popup)
-	}
-})()
+	document.body.appendChild(popup)
+}
